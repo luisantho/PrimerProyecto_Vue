274f35e05508519839c61db7707c152ff9f87629
luis anthony <anthonytoapanta15@gmail.com>
2023-09-11 10:11:19 +0200

    ahora si si

--- src/components/ApodComponent.vue
+++ src/components/ApodComponent.vue
@@ -1,39 +1,39 @@
 <template>
     <div class="container">
         <VueDatePicker v-model="date"></VueDatePicker>
-        <div class="row align-items-md-stretch" v-if="!responded">
+        <div class="row align-items-md-stretch" v-if="(result instanceof ErrorApod)">
             <div class="col-md-12">
                 <div class="h-100 p-5 text-black  border rounded-3">
-                    <h2>{{ errorCode }}</h2>
-                    <p>{{ errorMessage }}</p>
+                    <h2>{{ result.code }}</h2>
+                    <p>{{ result.msg  }}</p>
 
                 </div>
 
             </div>
         </div>
 
-        <div class="row align-items-md-stretch" v-if="responded">
+        <div class="row align-items-md-stretch" v-if="(result instanceof Apod)">
             <div class="col-md-12">
                 <div class="h-100 p-5 text-black  border rounded-3">
-                    <h2>{{ apod?.title }}</h2>
-                    <p>{{ apod?.date }}</p>
+                    <h2>{{ result?.title }}</h2>
+                    <p>{{ result?.date }}</p>
                     <div class="container-fluid d-flex">
-                        <img class="mx-auto" :src="apod?.url" :alt="apod?.title">
+                        <img class="mx-auto" :src="result?.url" :alt="result?.title">
 
                     </div>
-                    <p class="mt-3">{{ apod?.explanation }}</p>
+                    <p class="mt-3">{{ result?.explanation }}</p>
                 </div>
             </div>
-
+        
         </div>
     </div>
 </template>
 <script setup lang="ts" >
 import { ref, defineComponent, watch } from 'vue'
-import { Apod } from '@/models/ApodModel'
+import { Apod,ErrorApod,getApod } from '@/models/ApodModel'
 import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css'
-import { ErrorApod } from '@/models/ApodModel'
+
 
 defineComponent({
     components: {
@@ -41,48 +41,17 @@ defineComponent({
     }
 })
 
-const responded = ref(false)
-const errorMessage = ref('')
-const errorCode = ref(0)
-const date = ref('')
-const apod = ref<Apod>()
+const date = ref(new Date().toISOString().split('T')[0]);
+const result = ref<Apod | ErrorApod>()
 
 watch(date, async (newDate: any) => {
     console.log('new date' + newDate)
     //yyyy-mm-dd
     const dateStr = newDate.toISOString().split('T')[0];
-    await getApod(dateStr);
+    result.value = await getApod(dateStr);
 })
 
-
-const getApod = async (date?: string): Promise <ErrorApod | Apod> => {
-
-    let url = 'https://ap.nasa.gov/planetary/apod?api_key=DEMO_KEY'
-    if (date !== undefined) {
-        url += '&date=' + date
-    }
-    //Guardar la respuesta en una constante para poder poner condiciones
-    const response = await fetch(url)
-    if (!response.ok) {
-        console.error('Error' + JSON.stringify(response));
-        switch (response.status) {
-            case 400:
-                return new ErrorApod(400, 'Bad request')
-
-            case 500:
-                return new ErrorApod(500, 'Internal server error')
-            default:
-                break;
-        }
-
-    }
-    responded.value = true;
-    const json = await response.json();
-    return new Apod(json)
-
-}
-
-await getApod();
+result.value = await getApod();
 
 </script>
 
--- src/models/ApodModel.ts
+++ src/models/ApodModel.ts
@@ -103,3 +103,35 @@ export class ErrorApod{
         this._msg = value
     }
 }
+
+export const getApod = async (date?: string): Promise<ErrorApod | Apod> => {
+
+    let url = 'https://ap.nasa.gov/planetary/apod?api_key=DEMO_KEY'
+    if (date !== undefined) {
+        url += '&date=' + date
+    }
+    //Guardar la respuesta en una constante para poder poner condiciones
+    const response = await fetch(url)
+    if (!response.ok) {
+        console.error('Error' + JSON.stringify(response));
+        switch (response.status) {
+            case 400:
+                return new ErrorApod(400, 'Bad request')
+
+            case 500:
+                return new ErrorApod(500, 'Internal server error')
+            
+            case 404:
+                return new ErrorApod(404,'Not found')
+                
+            default:
+                break;
+        }
+
+    }
+    
+    const json = await response.json();
+    //console.log('json' +JSON.stringify(json))
+    return new Apod(json)
+
+}
