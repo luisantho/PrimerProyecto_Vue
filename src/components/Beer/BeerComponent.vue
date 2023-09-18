6f4b27b71bab978c3b47ca568b675dce738217fd
luis anthony <anthonytoapanta15@gmail.com>
2023-09-18 09:12:19 +0200

    slider añadido

--- src/components/Beer/BeerComponent.vue
+++ src/components/Beer/BeerComponent.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="container">
-        <Slider v-model="value" />
+        <Slider v-model="range" class="my-4" :lazy="false" />
     </div>
     <div class="container">
         <div class="row justify-content-center align-items-center g-2">
-            <div class="col-xs-12 col-md-6 col-lg-4" v-for="(beer, id) in beers" :key="id">
+            <div class="col-xs-12 col-md-6 col-lg-4" v-for="(beer, id) in filterBeers" :key="id">
                 <div class="card h-100">
                     <img class="card-img-top mx-auto mt-3" :src="beer.imageUrl" alt="Title" style="width: 60px;">
                     <div class="card-body">
@@ -19,28 +19,26 @@
     </div>
 </template>
 <script setup lang="ts" >
-import { ref, defineComponent,watch } from 'vue'
-import { Beer,getBeers } from '@/models/BeerModel'
+import { ref, defineComponent, computed } from 'vue'
+import { Beer, getBeers } from '@/models/BeerModel'
 import Slider from '@vueform/slider'
 
 defineComponent({
-    components:{
+    components: {
         Slider
     }
 })
 
-let value = ref(0)
+const range = ref([5, 10])
 const beers = ref<Beer[]>()
-
-watch(value,async (newValue:any) => {
-    console.log('new value' + newValue);
-    
-})
 
 beers.value = await getBeers();
 
+//Filtrando beers con un computed
+const filterBeers = computed(() => beers.value
+.filter(beer => beer.abv >= range.value[0] && beer.abv <= range.value[1])
+.sort((a,b) => a.abv - b.abv));
+
 </script>
 
-<style  src="@vueform/slider/themes/default.css">
-
-</style>
+<style  src="@vueform/slider/themes/default.css"></style>
